2af0e17cd0f78485d59b86e92cbe54d92d02c46d
gdmith <proar7188@gmail.com>
2026-02-12 02:22:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const cors = require("cors");
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 app.use(cors());
@@ -8,22 +8,30 @@ app.use(cors());
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-app.get("/token", async (req, res) => {
-  const auth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
-
-  const response = await fetch("https://accounts.spotify.com/api/token", {
+async function getToken() {
+  const res = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
-      "Authorization": `Basic ${auth}`,
-      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded"
     },
-    body: "grant_type=client_credentials",
+    body: "grant_type=client_credentials"
+  });
+
+  const data = await res.json();
+  return data.access_token;
+}
+
+app.get("/artist/:id", async (req, res) => {
+  const token = await getToken();
+  const artistId = req.params.id;
+
+  const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`, {
+    headers: { Authorization: `Bearer ${token}` }
   });
 
   const data = await response.json();
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
-});
+app.listen(3000, () => console.log("Server running"));
